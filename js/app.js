67651ce8c125f131cb18656bf6e814498bd870a0
dannypadilla <dpadilla010@gmail.com>
2019-05-21 14:33:31 -0700

    finish

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ const my_map = new Vue({
         this.lat = lat;
         this.lon = lon;
       }
-
+      // populate the map with lat/lon data
       show_map(parseFloat(this.lat), parseFloat(this.lon) )
       console.log(this.lat, this.lon);
     }
@@ -27,61 +27,27 @@ const my_map = new Vue({
 
 })
 
-
+// show interactive google map
 function show_map(latitude, longitude) {
   const uluru = {lat: latitude, lng: longitude};
 
   // default map view with a marker
-  var map_view = new google.maps.Map(
+  const map_view = new google.maps.Map(
     document.getElementById('map_display'), {
-      zoom: 18,
+      zoom: 15,
       center: uluru
     }
   );
 
-  var marker = new google.maps.Marker( {
+  const marker = new google.maps.Marker( {
     position: uluru,
     map: map_view // add map obj here
   });
 
-} // end of init_map
+} // end of show_map
 
 
-function mark_map(latitude, longitude) {
-  var map_find, infoWindow;
-  const uluru = {lat: latitude, lng: longitude};
-
-  map_find = new google.maps.Map(document.getElementById("map_display"), {
-    center: uluru,
-    zoom: 18
-  });
-  infoWindow = new google.maps.InfoWindow;
-  if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-          //infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          //infoWindow.open(map_find);
-          map_find.setCenter(pos);
-          var marker_c = new google.maps.Marker( {
-            position: pos,
-            map: map_find // add map obj here
-          });
-          marker_c.addListener("click", function() {
-            infoWindow.open(map_find, marker_c);
-          });
-      }, function() {
-          handleLocationError(true, infoWindow, map_find.getCenter() );
-      });
-  } else {
-      handleLocationError(false, infoWindow, map_find.getCenter() );
-  }
-} // end of mark_map
-
-
+// error handle from google
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(
